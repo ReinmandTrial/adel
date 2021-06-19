4a697079f1d4bc54fa862a92573ba2096d110eba
kirill.pr9n1k <kirill.pr9n1k@yandex.ru>
2021-06-19 11:50:05 +0300

    начало js

--- app/js/main.js
+++ app/js/main.js
@@ -54,3 +54,81 @@ document.querySelectorAll('.slider__container').forEach(function (elem) {
         },
     });
 });
+//Открытие блоков
+    $('.toggle-btn').on('click',function(){
+        var btn = $(this);
+        var block = btn.closest('.toggle-block');
+
+        btn.closest('.toggle-block').find('.toggle-body').slideToggle('slow');
+
+        block.find('.toggle-show').slideToggle('slow');
+
+        block.find('.toggle-icon').toggleClass('icon-triangle-down');
+        block.find('.toggle-icon').toggleClass('icon-triangle-up');
+    })
+
+    $('.toggle-btn--close').on('click',function(){
+        var btn = $(this);
+        var block = btn.closest('.toggle-block');
+        $(this).closest('.toggle-block').find('.toggle-body').slideToggle('slow');
+        block.find('.toggle-show').slideToggle('slow');
+    })
+//конец открытие блоков
+
+//select 
+    //select open-close
+    $('.select-header').on('click',function(){
+        var btn = $(this);
+        var block = btn.closest('.select');
+        block.find('.select-body').slideToggle();
+        block.find('.select-icon').toggleClass('icon-triangle-down');
+        block.find('.select-icon').toggleClass('icon-triangle-up');
+    })
+    //select open-close
+
+    //select выбор элемента
+    $('.select-item').on('click',function(){
+        var btn = $(this);
+        var block = btn.closest('.select');
+        var thisText = btn.text();
+        block.find('.select-title').text(thisText);
+
+        block.find('.select-input').val(thisText);
+
+        block.find('.select-body').slideToggle();
+        block.find('.select-icon').toggleClass('icon-triangle-down');
+        block.find('.select-icon').toggleClass('icon-triangle-up');
+    })
+//select end
+
+// сердечко
+    $('.heart-item').on('click',function(){
+        $(this).closest('.heart-block').find('.heart-item').toggleClass('active');
+    })
+// сердечко конец
+
+//count 
+$(function(){
+    $('body').on('input', '.count-input', function(){
+       this.value = this.value.replace(/[^0-9]/g, '');
+    });
+ })
+ $('.count-mine').on('click',function(){
+     var val = $(this).closest('.count-block').find('.count-input').val();
+     if(val > 0 ){
+        $(this).closest('.count-block').find('.count-input').val(parseInt(val) - 1);
+     }
+ })
+ $('.count-more').on('click',function(){
+     var val = $(this).closest('.count-block').find('.count-input').val();
+     if(val < 999){
+        $(this).closest('.count-block').find('.count-input').val(parseInt(val) + 1);
+     }
+ })
+//count end
+
+//удаление карточки
+ $('.card-del').on('click',function(){
+     $(this).closest('.card-block').remove();
+ })
+//удаление карточки конец
